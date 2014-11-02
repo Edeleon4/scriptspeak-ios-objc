edb8d0e7d7ca7e8238d25a2f925e935eb0cc4a6d
Your Namels <edeleon4@mit.edu>
2014-11-02 17:11:48 -0500

    Initial Commit

---
+++ CrowdSpeechObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrowdSpeechObjC
+//
+//  Created by Eduardo de Leon on 11/2/14.
+//  Copyright (c) 2014 Eduardo de Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrowdSpeechObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrowdSpeechObjC
+//
+//  Created by Eduardo de Leon on 11/2/14.
+//  Copyright (c) 2014 Eduardo de Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
